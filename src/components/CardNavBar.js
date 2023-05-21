1e0b3103201d54d3f921f49902feeb64711e1c86
samanthamarberger <slatham04@gmail.com>
2023-05-20 18:34:38 -0600

    Still trying to get new page to load from link click

--- src/components/CardNavBar.js
+++ src/components/CardNavBar.js
@@ -1,14 +1,18 @@
 import React from "react";
-import { Link } from "react-router-dom";
+import { NavLink } from "react-router-dom";
 
 function CardNavBar({ exercise, muscleGroup }) {
-    // console.log(muscleGroup);
     return(
         <div className="cardNavbar"> 
-            <nav>
-                <Link to={`/muscle-groups/${muscleGroup.name}/${exercise.name}`} className={`${exercise.name}`}>{`${exercise.name}`}</Link>
-            </nav>
-        </div>
+      <nav>
+        <NavLink
+          to={`/muscle-groups/${muscleGroup.name}/${exercise.name}`}
+          className={`${exercise.name}`}
+        >
+          {`${exercise.name}`}
+        </NavLink>
+      </nav>
+    </div>
     )
 }
 
